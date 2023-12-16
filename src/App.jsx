c24731e2b6725042e1886877268916f89930bce1
Becksensei <as.atsadayut@gmail.com>
2023-12-17 00:36:56 +0700

    add router in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
 import Users from './component/Users'
 import Navbar from './component/navbar'
+import UserCreate from './component/UserCreate'
 
 function App() {
-
   return (
     <div>
-        <Navbar />
-        <Users />
+      <Navbar />
+      <Routes>
+        <Route path ="/" element={<Users />} />
+        <Route path ="Create" element={<UserCreate />} />
+    </Routes>
     </div>
-  )
+  );
 }
 
 export default App
--- src/component/Users.jsx
+++ src/component/Users.jsx
@@ -12,6 +12,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Avatar from '@mui/material/Avatar';
+import Link from '@mui/material/Link';
 
 export default function Users() {
   const [items, setItems] = useState([]);
@@ -38,7 +39,9 @@ export default function Users() {
               </Typography>
             </Box>
             <Box>
-              <Button variant="contained">Create</Button>
+              <Link href="create">
+                <Button variant="contained">Create</Button>
+              </Link>
             </Box>
           </Box>
 
@@ -76,7 +79,6 @@ export default function Users() {
               </TableBody>
             </Table>
           </TableContainer>
-
         </Paper>
       </Container>
     </React.Fragment>
