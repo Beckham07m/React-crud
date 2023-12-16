8c108c22bf2dbc58e08bbefa78bfb3dec9d10bb0
Becksensei <as.atsadayut@gmail.com>
2023-12-16 21:55:48 +0700

    create crud

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Users from './component/Users'
+import Navbar from './component/navbar'
+
+function App() {
+
+  return (
+    <div>
+        <Navbar />
+        <Users />
+    </div>
+  )
+}
+
+export default App
