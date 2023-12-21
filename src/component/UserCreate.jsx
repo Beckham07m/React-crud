223b2241796b8c37526c57d0c31e1a5f2a332680
Becksensei <as.atsadayut@gmail.com>
2023-12-21 18:23:57 +0700

    Add the Delete function

--- src/component/UserCreate.jsx
+++ src/component/UserCreate.jsx
@@ -1,9 +1,46 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import { Grid, Typography, TextField, Button, Card, CardContent } from '@mui/material';
 
 export default function UserCreate() {
+  const handleSubmit = event => {
+    event.preventDefault();
+    var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+  "fname": fname,
+  "lname": lname,
+  "username": username,
+  "email": email,
+  "avatar": avatar
+});
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://www.melivecode.com/api/users/create", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    alert(result['message'])
+    if (result['status'] === 'ok') {
+      window.location.href = '/'
+    }
+  })
+  .catch(error => console.log('error', error));
+  }
+
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [avatar, setAvatar] = useState('');
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -12,30 +49,35 @@ export default function UserCreate() {
             Create Users
           </Typography>
           <Card>
-          <CardContent>
-          <form >
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                  <TextField id="fname" label="First Name" variant="outlined" fullWidth required />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                  <TextField id="lname" label="Last Name" variant="outlined" fullWidth required />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                  <TextField id="username" label="Username" variant="outlined" fullWidth required />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                  <TextField id="email" label="Email" variant="outlined" fullWidth required />
-              </Grid>
-              <Grid item xs={12}>
-                  <TextField id="avatar" label="Avatar" variant="outlined" fullWidth required />
-              </Grid>
-              <Grid item xs={12} align="right">
-                  <Button variant="contained" >Create</Button>
-              </Grid>
-            </Grid>
-          </form>
-          </CardContent>
+            <CardContent style={{ border: '2px solid #007bff', borderColor: '#007bff' }}>
+              <form onSubmit={handleSubmit}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                      <TextField id="fname" label="First Name" variant="outlined" fullWidth required 
+                      onChange={(e) => setFname(e.target.value)}/>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                      <TextField id="lname" label="Last Name" variant="outlined" fullWidth required 
+                      onChange={(e) => setLname(e.target.value)}/>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                      <TextField id="username" label="Username" variant="outlined" fullWidth required 
+                      onChange={(e) => setUsername(e.target.value)}/>
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                      <TextField id="email" label="Email" variant="outlined" fullWidth required 
+                      onChange={(e) => setEmail(e.target.value)}/>
+                  </Grid>
+                  <Grid item xs={12}>
+                      <TextField id="avatar" label="Avatar" variant="outlined" fullWidth required 
+                      onChange={(e) => setAvatar(e.target.value)}/>
+                  </Grid>
+                  <Grid item xs={12} align="right">
+                      <Button type='submit' variant="contained">Create</Button>
+                  </Grid>
+                </Grid>
+              </form>
+            </CardContent>
           </Card>
         </Container>
     </React.Fragment>
--- src/component/Users.jsx
+++ src/component/Users.jsx
@@ -13,11 +13,16 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Avatar from '@mui/material/Avatar';
 import Link from '@mui/material/Link';
+import ButtonGroup from '@mui/material/ButtonGroup'
 
 export default function Users() {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
+    UserGet()
+  }, [])
+
+  const UserGet = () => {
     fetch("https://www.melivecode.com/api/users")
       .then(res => res.json())
       .then(
@@ -25,7 +30,33 @@ export default function Users() {
           setItems(result);
         },
       )
-  }, [])
+  }
+
+  const UserDelete = id => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      "id": id
+    });
+
+    var requestOptions = {
+      method: 'DELETE',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+};
+
+fetch("https://www.melivecode.com/api/users/delete", requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    alert(result['message'])
+    if (result['status'] === 'ok') {
+      UserGet()
+    }
+  })
+  .catch(error => console.log('error', error));
+  }
 
   return (
     <React.Fragment>
@@ -73,7 +104,12 @@ export default function Users() {
                     <TableCell align="right">{row.fname}</TableCell>
                     <TableCell align="right">{row.lname}</TableCell>
                     <TableCell align="right">{row.username}</TableCell>
-                    <TableCell align="right"></TableCell>
+                    <TableCell align="right">
+                      <ButtonGroup variant="outlined" aria-label="outlined button group">
+                        <Button>Edit</Button>
+                        <Button onClick={() => UserDelete(row.id)}>Del</Button>
+                      </ButtonGroup>
+                    </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
